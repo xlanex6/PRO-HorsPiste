9ae070a5cb663de8189bd58ef407fa4cdae2a9f7
Alex <alexduval71@gmail.com>
2017-03-24 19:09:51 +0100

    index, new and create on Run works with no-design

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -5,16 +5,42 @@ class RunsController < ApplicationController
   end
 
   def index
-    @run = Run.all
+    @runs = Run.all
   end
 
   def new
     @run = Run.new
   end
 
+  def create
+    @run = Run.new(run_params)
+    if @run.save
+      redirect_to @run, notice: "Votre itinéraire a bien été ajouté"
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_run
     @run = Run.find(params[:id])
+  end
+
+  def run_params
+    params.require(:run).permit(  :name,
+                                  :technic,
+                                  :physical,
+                                  :engagement,
+                                  :run_type,
+                                  :best_snow,
+                                  :best_time,
+                                  :expo,
+                                  :alt_start,
+                                  :alt_finish,
+                                  :description,
+                                  :area_id,
+                                  :resort_id
+                                )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324130748) do
+ActiveRecord::Schema.define(version: 20170324173515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20170324130748) do
     t.integer  "technic"
     t.integer  "physical"
     t.string   "engagement"
-    t.string   "type"
+    t.string   "run_type"
     t.string   "best_snow"
     t.string   "best_time"
     t.string   "expo"
