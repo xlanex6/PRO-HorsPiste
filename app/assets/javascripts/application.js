c82e0d320453db9932d5aa5406e787b93b5ff98d
Alex <alexduval71@gmail.com>
2017-03-31 17:53:38 +0200

    Add List.js and search with it on index#run

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require listjs
 //= require_tree .
+
+
+var monkeyList = new List('run-list', {
+  valueNames: ['run-name', 'run-area', 'run-resort']
+});
